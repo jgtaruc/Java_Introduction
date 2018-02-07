30bbeca6508caea13a6c730b5e8c20062086ce3c
jgtaruc <jgtaruc@gmail.com>
2018-02-06 21:58:41 -0800

    Learning the basics

---
+++ 007_Objects/Main.java
@@ -0,0 +1,31 @@
+//Write a new method in Point called scale, that will make the point closer by half to (0,0). So for example, point (8, 4) after scale will be (4, 2).
+
+class Point {
+    private double x;
+    private double y;
+    
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+    
+    public void print() {
+        System.out.println("(" + x + "," + y + ")");
+    }
+    
+    // Your code goes here
+    public void scale() {
+        this.x /= 2;
+        this.y /= 2;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Point p = new Point(32, 32);
+        for (int i = 0; i < 5; i++) {
+            p.scale();
+            p.print();
+        }
+    }
+}
---
+++ 008_Arguments/Main.java
@@ -0,0 +1,10 @@
+//Create a program that prints the arguments of our program. Write one argument per line.
+
+public class Main {
+    public static void main(String[] args) {
+        // write your code here
+        for(String arg : args) {
+            System.out.println(arg);
+        }
+    }
+}
